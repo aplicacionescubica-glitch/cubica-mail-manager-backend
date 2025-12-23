5038706abb3405e84706fa5d12ecc22c087a37c2
UsuarioLocal <aplicacionescubica@gmail>
2025-12-23 17:56:38 -0500

    correcion de import middleware

--- src/modules/gmail/emailLog.routes.js
+++ src/modules/gmail/emailLog.routes.js
@@ -2,7 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 const { listEmailHistory } = require("./emailLog.controller");
-const authMiddleware = require("../../middlewares/auth.middleware");
+
+// Import robusto del middleware (soporta export directo o { authMiddleware })
+const auth = require("../../middlewares/auth.middleware");
+const authMiddleware = auth && typeof auth === "function" ? auth : auth?.authMiddleware;
 
 /* Rutas de historial de correos */
 router.get("/history", authMiddleware, listEmailHistory);
