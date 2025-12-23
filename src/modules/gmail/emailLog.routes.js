3721a9b4d5051f943308af96f2426ab0376795cf
UsuarioLocal <aplicacionescubica@gmail>
2025-12-23 17:59:25 -0500

    Modificacion Export auth.middleware

--- src/modules/gmail/emailLog.routes.js
+++ src/modules/gmail/emailLog.routes.js
@@ -2,12 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const { listEmailHistory } = require("./emailLog.controller");
-
-// Import robusto del middleware (soporta export directo o { authMiddleware })
-const auth = require("../../middlewares/auth.middleware");
-const authMiddleware = auth && typeof auth === "function" ? auth : auth?.authMiddleware;
+const { requireAuth } = require("../../middlewares/auth.middleware");
 
 /* Rutas de historial de correos */
-router.get("/history", authMiddleware, listEmailHistory);
+router.get("/history", requireAuth, listEmailHistory);
 
 module.exports = router;
