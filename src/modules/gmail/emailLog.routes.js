bc4d8417cef18d01ffb2835a16c9af2e431e6813
UsuarioLocal <aplicacionescubica@gmail>
2025-12-23 17:44:43 -0500

    correcion import auth.middleware

--- src/modules/gmail/emailLog.routes.js
+++ src/modules/gmail/emailLog.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const { listEmailHistory } = require("./emailLog.controller");
-const authMiddleware = require("../../middlewares/authMiddleware");
+const authMiddleware = require("../../middlewares/auth.middleware");
 
 /* Rutas de historial de correos */
 router.get("/history", authMiddleware, listEmailHistory);
